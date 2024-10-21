fc4c287fc342e55252fba3b89d706aafb32fc910
Mehdi <mehdi.teisseire@laplateforme.io>
2024-10-21 18:40:32 +0200

    add: job6

--- job5.py
+++ job5.py
@@ -1,1 +1,8 @@
-print("test")
+def travel_time(x,y):
+    
+    marche = x
+    week=(marche *2)*7
+    distance = week * y
+    print(f"vous avez parcouru cette semaine {(distance*5)/100} metres")
+
+travel_time(20,30)
